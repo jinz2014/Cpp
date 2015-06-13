bc7be587d9cf4ffb3b453111c216bf88f2bab8b1
jinz <jinz@email.sc.edu>
2015-06-13 15:23:06 -0400

    show basic, advanced, extended examples

--- algorithm/for_each.cpp
+++ algorithm/for_each.cpp
@@ -1,4 +1,4 @@
-// for_each example
+// STL for_each example
 #include <iostream>     // cout
 #include <algorithm>    // for_each
 #include <vector>       // vector
@@ -6,6 +6,13 @@
 #include <fstream>       // vector
 
 using namespace std;
+
+/*
+   function object is passed instead of a function pointer(in C).
+   When invoked, the callback function is executed just as any
+   other member function, and therefore has full access to the
+   other members (data or functions) of the object.
+ */
 
 void myStringFunction (string s) {  // function:
     cout << ' ' << s + ";";
@@ -28,12 +35,64 @@ class myclass {           // function object type:
     }
 };
 
+
+// The function object multiplies an element by a Factor
+template <class Type>
+class MultValue
+{
+  private:
+     Type Factor;   // The value to multiply by
+  public:
+     // Constructor initializes the value to multiply by
+     MultValue ( const Type& _Val ) : Factor ( _Val ) {
+     }
+
+     // The function call for the element to be multiplied
+     void operator ( ) ( Type& elem ) const
+     {
+        elem *= Factor;
+     }
+};
+
+// The function object to determine the average
+class Average
+{
+  private:
+     long num;      // The number of elements
+     long sum;      // The sum of the elements
+  public:
+     // Constructor initializes the value to multiply by
+     Average ( ) : num ( 0 ) , sum ( 0 )
+     {
+     }
+
+     // The function call to process the next elment
+     void operator ( ) ( int elem ) 
+     {
+        num++;      // Increment the element count
+        sum += elem;   // Add the value to the partial sum
+     }
+
+     // return Average
+     operator double ( )
+     {
+        return  static_cast<double> (sum) / static_cast<double> (num);
+     }
+};
+
+
+
 int main () {
   vector<int> myvector;
   myvector.push_back(10);
   myvector.push_back(20);
   myvector.push_back(30);
 
+  //----------------------------------------------------------------------
+  // Basic example
+  //----------------------------------------------------------------------
+  // this is straigtform as each vector element is passed as an argument
+  // to the function "myfunction"
   cout << "myvector contains:";
   for_each (myvector.begin(), myvector.end(), myfunction); // function pointer
   cout << '\n';
@@ -42,10 +101,57 @@ int main () {
   for_each (myvector.begin(), myvector.end(), myobject); // struct
   cout << '\n';
 
-  //myclass f = myclass();
   cout << "myvector contains:";
   for_each (myvector.begin(), myvector.end(), myclass()); // class
   cout << '\n';
+
+  //----------------------------------------------------------------------
+  // Advanced example
+  //----------------------------------------------------------------------
+   vector <int> v1;
+   vector <int>::iterator Iter1;
+
+   // Constructing vector v1
+   int i;
+   for ( i = -4 ; i <= 2 ; i++ )
+   {
+      v1.push_back(  i );
+   }
+
+   cout << "Original vector  v1 = ( " ;
+   for ( Iter1 = v1.begin( ) ; Iter1 != v1.end( ) ; Iter1++ )
+      cout << *Iter1 << " ";
+   cout << ")." << endl;
+
+   // Using for_each to multiply each element by a Factor
+   for_each ( v1.begin ( ) , v1.end ( ) , MultValue<int> ( -2 ) );
+
+   cout << "Multiplying the elements of the vector v1\n "
+        <<  "by the factor -2 gives:\n ( " ;
+   for ( Iter1 = v1.begin( ) ; Iter1 != v1.end( ) ; Iter1++ )
+      cout << *Iter1 << " ";
+   cout << ")." << endl;
+
+   // The function object is templatized and so can be
+   // used again on the elements with a different Factor
+   for_each (v1.begin ( ) , v1.end ( ) , MultValue<int> (5 ) );
+
+   cout << "Multiplying the elements of the vector v1\n "
+        <<  "by the factor 5 gives:\n ( " ;
+   for ( Iter1 = v1.begin( ) ; Iter1 != v1.end( ) ; Iter1++ )
+      cout << *Iter1 << " ";
+   cout << ")." << endl;
+
+   // The local state of a function object can accumulate
+   // information about a sequence of actions that the
+   // return value can make available, here the Average
+   double avemod2 = for_each ( v1.begin ( ) , v1.end ( ) , Average ( ) );
+   cout << "The average of the elements of v1 is:\n Average ( v1mod2 ) = "
+        << avemod2 << "." << endl;
+
+  //-----------------------------------------------------------------
+  // Extended example
+  //-----------------------------------------------------------------
 
   // display input file
   typedef istream_iterator<string> in;
